04b68926f5dc20b32ed83a873086c980d729078d
Emre Bilgiç <emrevebilgic@gmail.com>
2023-03-16 23:22:00 +0300

    effect hook example

--- src/pages/EffectHook.js
+++ src/pages/EffectHook.js
@@ -1,10 +1,57 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
+
+const LittleComponent = (props) => {
+
+    useEffect( ()=> {
+        if(props.tickleCount === 0)
+            console.log("The little component is born...")
+        return ()=> console.log("You destroyed the little component you heartless monster!")
+    },[])
+
+    useEffect( ()=> {
+        //This hook will run everytime the compenent is created, everytime it is tickled AND will run the return function everytime it is killed
+        if(props.tickleCount > 0) {
+            console.log("You tickled the little component.")
+        }
+    }, [props.tickleCount])
+
+    return (
+        <div className='bg-secondary color-container mb-3'>
+            { props.tickleCount === 0 ?
+                "I am the little component and I am very cute. Please don't kill me."
+                :
+                `Hoohoo you just tickled me ${props.tickleCount} times!`
+            }
+        </div>
+    )
+}
 
 const EffectHook = () => {
+
+    const [count, setCount] = useState(0);
+    const [show, setShow] = useState(true);
+
+    function killIt() {
+        setShow((s)=>!s);
+        setCount(0);
+    }
+
     return (
-        <p>
-            This is gonna be the effect hook example
-        </p>
+        <>
+            <p>
+                useEffect is out lifecycle management hook. Depending on how you pass the arguments or assigning a return function, this hook acts in several ways. We can make our code do things when a component is <strong>created, updated or killed.</strong> Please check the code and read the console to understand changes and triggers:
+            </p>
+            <div className='mb-3'>
+                { show &&
+                    <LittleComponent tickleCount={count}/>
+                }
+                <button className='btn btn-danger' onClick={()=> killIt()}>{show ? "Kill!" : "Oops."}</button>
+                {show &&
+                    <button className='btn btn-primary ms-2' onClick={()=> setCount((c)=>c+1)}> Tickle! </button>
+                } 
+            </div>
+        </>
     )
 }
 
--- src/pages/StateHook.js
+++ src/pages/StateHook.js
@@ -9,16 +9,16 @@ const StateHook = () => {
             <p>
                 useState is the simplest of our hooks. It is the hook to manipulate a component's states. Each state that is bound to a useState hook can be assigned to a new value by the setter function also provided by the hook. As you know, when a component's state is changed, a re-render happens... Lets see it below:
             </p>
-            <div className={`bg-${color} color-container mx-2 mb-3`}>
-               {`My color is ${color}`}
-            </div>
-            <div>
-                <button className='btn btn-primary mx-2 mb-2' onClick={ ()=> setColor("primary") }>Primary</button>
+
+            <div className="d-inline-block mb-3">
+                <div className={`bg-${color} w-100 color-container mb-3`}>
+                {`My color is ${color}`}
+                </div>
+                <button className='btn btn-primary me-2 mb-2' onClick={ ()=> setColor("primary") }>Primary</button>
                 <button className='btn btn-secondary mx-2 mb-2' onClick={ ()=> setColor("secondary") }>Secondary</button>
                 <button className='btn btn-warning mx-2 mb-2' onClick={ ()=> setColor("warning") }>Warning</button>
-                <button className='btn btn-danger mx-2 mb-2' onClick={ ()=> setColor("danger") }>Danger</button>
+                <button className='btn btn-danger ms-2 mb-2' onClick={ ()=> setColor("danger") }>Danger</button>
             </div>
-
         </>
     )
 }
